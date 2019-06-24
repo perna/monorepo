c58fc69d2b26f02df18bdf4d9b48f73494f843c8
Anderson Meira <anderson.meira@gmail.com>
2019-06-24 10:18:43 -0300

    test new config 2

--- packages/awesome/index.js
+++ packages/awesome/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Hello = () => { 
     console.log('Hello World')
+
+    const newFunction = ('teste') => {
+        return 1 + 1
+    }
 }
 
 export default Hello
